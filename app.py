b1057823807366eedee050f9945f72fd1a6a4543
Dharun AP <121315919+DharunAP@users.noreply.github.com>
2024-06-18 17:16:09 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,24 @@
+from flask import Flask, render_template
+
+app = Flask(__name__,template_folder='templates',static_folder = 'static',static_url_path='/')
+
+@app.route('/')
+def home():
+    table_data = [
+        'images/1.jpg' ,
+        'images/2.jpg' ,
+        'images/3.jpg' ,
+        'images/4.jpg' ,
+        'images/5.png' ,
+        'images/6.jpg' ,
+        'images/7.jpeg' ,
+    ]
+
+    return render_template('index.html', table_data=table_data)
+
+@app.route('/upload')
+def hello():
+    return render_template('upload.html')
+
+if __name__ == '__main__':
+    app.run()
